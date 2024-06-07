9d8e46112f5790d9769795eb53b15e25dea1ab8c
mattOd80 <mod80.projects@gmail.com>
2024-06-06 23:35:54 -0400

    trying some simple react...

--- spa.js
+++ spa.js
@@ -1,1 +1,5 @@
-const ResumeBuilder = () => <div id="your-resume">Some really awesome content</div>
+const App = ({ org_html, children }) => <div id="your-resume">
+    Some really awesome content
+    <div id='org-root' dangerouslySetInnerHTML={{ __html: org_html.replace('class="entry"', 'class="entry entry-spa"') }} />
+    { children }
+</div>
