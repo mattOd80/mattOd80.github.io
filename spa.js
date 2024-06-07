1acd1660e5f320542fda101359483fcdfe5fd47b
mattOd80 <mod80.projects@gmail.com>
2024-06-07 03:08:58 -0400

    ignore

--- spa.js
+++
@@ -1,7 +0,0 @@
-const App = ({ org_html, children }) => {
-    return <div id="your-resume">
-        Some really awesome content
-        <div id='org-root' dangerouslySetInnerHTML={{ __html: org_html.replace('class="entry"', 'class="entry entry-spa"') }} />
-        {children}
-    </div>;
-}
