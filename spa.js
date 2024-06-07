c6ccc8baccf4591f3615838796f71a89918724a2
mattOd80 <mod80.projects@gmail.com>
2024-06-06 23:15:33 -0400

    trying some simple react

--- spa.js
+++ spa.js
@@ -1,10 +1,1 @@
-// Define a component called Greetings
-function Greetings() {
-  return <h1>Hello readers, Thankyou for reading this blog !</h1>;
-}
-
-
-// Render the component to the DOM
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container); 
-root.render(<Greetings />);
+const ResumeBuilder = () => <div id="your-resume">Some really awesome content</div>
