e00cf18e4fdc7ddb3cd2f5396372c1c025c103aa
mattOd80 <mod80.projects@gmail.com>
2024-06-06 21:57:31 -0400

    cdn based spa setup and render

--- spa.js
+++ spa.js
@@ -1,1 +1,10 @@
-alert('Script loaded');
+// Define a component called Greetings
+function Greetings() {
+  return <h1>Hello readers, Thankyou for reading this blog !</h1>;
+}
+
+
+// Render the component to the DOM
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container); 
+root.render(<Greetings />);
